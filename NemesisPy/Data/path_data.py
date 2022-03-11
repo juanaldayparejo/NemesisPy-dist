f1e79ce49f2bb07a199640577b1d3664afc5f526
juanaldayparejo <juanaldayparejo@gmail.com>
2022-03-11 13:18:34 +0000

    automatic identification of NemesisPy path

--- NemesisPy/Data/path_data.py
+++ NemesisPy/Data/path_data.py
@@ -1,3 +1,6 @@
 def Nemesis_Path():
-    nemesis_path = '/Users/aldayparejo/Documents/Projects/PlanetaryScience/NemesisPy-dist/'
+    import os
+    #nemesis_path = '/home/juanalday/Documents/Projects/NemesisPy-dist/'
+    #nemesis_path = os.path.dirname(os.path.realpath(__file__))
+    nemesis_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../../')
     return nemesis_path
