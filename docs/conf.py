979eb9502b6e6209bf79c4b593422e4780e7b539
juanaldayparejo <juanaldayparejo@gmail.com>
2022-06-22 07:46:38 +0100

    add images in gallery

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,9 @@ extensions = [
 ]
 
 nbsphinx_thumbnails = {
-    'examples/mars_solocc/mars_SO': '_static/mars_tgo.jpg',
+    'examples/mars_solocc/mars_SO': '_static/exomars_SO.jpg',
+    'examples/Jupiter_CIRS_nadir_thermal_emission/Jupiter_CIRS': '_static/jupiter_cassini.jpg',
+    'examples/Measurement/Measurement': 'examples/Measurement/observation_sketch.png'
 }
 
 html_theme = 'sphinx_rtd_theme'
