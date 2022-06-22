2b53589f0a2ce2d90050fbc336ad05e047c7979a
juanaldayparejo <juanaldayparejo@gmail.com>
2022-06-22 19:57:42 +0100

    adding figures to gallery

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,10 @@ extensions = [
 nbsphinx_thumbnails = {
     'examples/mars_solocc/mars_SO': '_static/exomars_SO.jpg',
     'examples/Jupiter_CIRS_nadir_thermal_emission/Jupiter_CIRS': '_static/jupiter_cassini.jpg',
-    'examples/Measurement/Measurement': 'examples/Measurement/observation_sketch.png'
+    'examples/Measurement/Measurement': '_static/observation_sketch.png',
+    'examples/Stellar/StellarExample': '_static/stellar_spectrum.jpg',
+    'examples/Stellar/StellarExample': '_static/stellar_spectrum.jpg',
+    'examples/Atmosphere/Atmosphere': '_static/planetary_atmospheres.png'
 }
 
 html_theme = 'sphinx_rtd_theme'
