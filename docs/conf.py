21fe8625a693afa6283634b579b38a17b97413ab
juanaldayparejo <juanaldayparejo@gmail.com>
2022-06-23 09:06:44 +0100

    changing the style of the notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@ extensions = [
     "sphinx_gallery.load_style",
 ]
 
+
+#Defining each image shown in the gallery
 nbsphinx_thumbnails = {
     'examples/mars_solocc/mars_SO': '_static/exomars_SO.jpg',
     'examples/Jupiter_CIRS_nadir_thermal_emission/Jupiter_CIRS': '_static/jupiter_cassini.jpg',
@@ -14,48 +16,18 @@ nbsphinx_thumbnails = {
     'examples/MieScattering/MieScattering': '_static/mars_sunset2.jpg'
 }
 
+
+#Defining the actual appearance of the website
 html_theme = 'sphinx_rtd_theme'
 
 
-
-# This is processed by Jinja2 and inserted before each notebook
-nbsphinx_prolog = r"""
-{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
+#For changing the style of the jupyter notebooks
+nbsphinx_prolog = """
 .. raw:: html
-    <div class="admonition note">
-      This page was generated from
-      <a class="reference external" href="https://github.com/spatialaudio/nbsphinx/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
-      Interactive online version:
-      <a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.
-      <script>
-        if (document.location.host) {
-          $(document.currentScript).replaceWith(
-            '<a class="reference external" ' +
-            'href="https://nbviewer.jupyter.org/url' +
-            (window.location.protocol == 'https:' ? 's/' : '/') +
-            window.location.host +
-            window.location.pathname.slice(0, -4) +
-            'ipynb">View in <em>nbviewer</em></a>.'
-          );
+
+    <style>
+        h1 {
+            color: chartreuse;
         }
-      </script>
-    </div>
-.. raw:: latex
-    \nbsphinxstartnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{The following section was generated from
-    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+    </style>
 """
-
-# This is processed by Jinja2 and inserted after each notebook
-nbsphinx_epilog = r"""
-{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
-.. raw:: latex
-    \nbsphinxstopnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
-    {{ docname | escape_latex }}}} ends here.}}
-"""
-
-mathjax_config = {
-    'TeX': {'equationNumbers': {'autoNumber': 'AMS', 'useLabelIds': True}},
-}
-
