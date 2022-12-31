091006a332eecfc8e44549d06a7935b00864fed4
juanaldayparejo <67833635+juanaldayparejo@users.noreply.github.com>
2022-12-31 08:46:44 +0100

    Enable mathjax in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
 #    "sphinx.ext.viewcode",
     "IPython.sphinxext.ipython_console_highlighting",
     ]
