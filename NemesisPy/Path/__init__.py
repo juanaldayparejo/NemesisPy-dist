f12f35c0ad32ad92f876fd6c254f368811aa78e9
Juan Alday <juanaldayparejo@gmail.com>
2024-01-24 10:34:30 +0000

    cleaned code

--- NemesisPy/Path/__init__.py
+++ NemesisPy/Path/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 from .Measurement_0 import *
-from .AtmCalc_0 import AtmCalc_0
-from .Path_0 import Path_0
-from .Path import *
-from .calc_path import *
-from .calc_pathg import *
-from .calc_path_SO import *
-from .calc_pathg_SO import *
+from .AtmCalc_0 import *
+from .Path_0 import *
